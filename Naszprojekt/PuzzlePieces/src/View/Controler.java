b6dcadafd4c66816c6c4cfaaf7f567967aa3bed3
kuba1803 <kuba1803@ymail.com>
2014-05-08 09:13:16 +0200

    new change

---
+++ Naszprojekt/PuzzlePieces/src/View/Controler.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package View;
+
+/**
+ *
+ * @author kuba1_000
+ */
+public class Controler {
+    public Model model;
+    //public View view;
+    /*public Controler(Model model, View view)
+    {
+        this.model = model;
+        this.view = view;
+        
+        this.view.registerPieceSelected( 
+    }
+    
+    public void pieceSelected( ){
+    }
+    
+    public void piecePasted( ) {
+    }
+    
+    
+    }*/
+    public Controler()
+    {
+        model=new Model();
+    }
+    
+}
---
+++ Naszprojekt/PuzzlePieces/src/View/Table.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package View;
+
+import View.Field;
+
+/**
+ *
+ * @author kuba1_000
+ */
+public class Table
+{
+     Field[][] table;
+     public Table(int x,int y)
+     {
+         table= new Field[x][y];
+         for(int i=0;i<x;i++)
+         {
+             for(int j=0;j<y;j++)table[i][j]=new Field();
+         }
+     }
+     void set(Shallow a, int x, int y)
+     {
+         table[x][y].set(a);
+     }
+     public Field get(int x,int y)
+     {
+         return table[x][y];
+     }
+     public boolean check(int x,int y)
+    {
+       return table[x][y].check();
+    }
+}
